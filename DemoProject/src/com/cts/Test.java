63e1994137370620bcf0254f4528e661a67b6c03
CGI <CGI@CGI-JAVA-128>
2019-05-09 11:17:22 +0530

    Second Message for use of Git

--- DemoProject/src/com/cts/Test.java
+++ DemoProject/src/com/cts/Test.java
@@ -3,5 +3,6 @@ package com.cts;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello git");
+		System.out.println("Hi git I am Biswajit going to use you");
 	}
 }
